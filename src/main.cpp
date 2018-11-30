36499166baa1cf97a35290edb31a97b59a7fb07b
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-11-30 01:12:31 +0100

    Add shuffle back in

--- src/main.cpp
+++ src/main.cpp
@@ -61,8 +61,7 @@ int main() {
         auto s1 = static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count());
         std::seed_seq seed{s1};
         std::mt19937 mt(seed);
-        //this causes a crash apparently
-        //std::shuffle(botStats.begin(), botStats.end(), mt);
+        std::shuffle(botStats.begin(), botStats.end(), mt);
 
         //draw players to play and start game
         std::vector<Player> players;
