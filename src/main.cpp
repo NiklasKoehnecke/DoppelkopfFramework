4573a918dd78eefb90c40c7eb2fae6adade6e915
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2018-11-30 01:06:08 +0100

    Successfully write and read to file

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,81 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <algorithm>
+#include <chrono>
+
 #include "core/Game.h"
 #include "exampleBot/ExampleBot.h"
 
 template<typename T>
-Player* createInstance() { return new T; }
+Player *createInstance() { return new T; }
 
-typedef std::map<std::string, Player*(*)()> map_type;
+typedef std::map<std::string, Player *(*)()> map_type;
 
 map_type map;
-void createPlayerMapping(){
+
+void createPlayerMapping() {
     map.insert(std::make_pair("ExampleBot", &createInstance<ExampleBot>));
 }
 
+std::vector<std::pair<std::string, int>> readStats(std::fstream &file) {
+
+    int numPlayers;
+    //read all player data
+    std::vector<std::pair<std::string, int>> botStats;
+    file >> numPlayers;
+    std::string player;
+    int points;
+    for (int i = 0; i < numPlayers; i++) {
+        file >> player;
+        file >> points;
+        botStats.push_back(std::make_pair(player, points));
+    }
+    return std::move(botStats);
+
+}
+
+void writeStats(std::fstream &file, std::vector<std::pair<std::string, int>> &stats) {
+    file.clear();
+    file.seekp(0);
+    file << stats.size() << "\n";
+    for (auto playerStat: stats) {
+        file << playerStat.first << " " << playerStat.second << "\n";
+    }
+    file.flush();
+    file.close();
+}
+
+
 int main() {
     std::string filename = "../player.csv";
-    std::fstream file("../player.csv");
+    std::fstream file("../player.csv" , file.in | file.out);
 
     if (!file.is_open()) {
         std::cout << "failed to open " << filename << '\n';
     } else {
         createPlayerMapping();
-        std::string p1, p2 ,p3 ,p4;
-        file >> p1 >> p2 >> p3 >> p4;
 
+        std::vector<std::pair<std::string, int>> botStats = readStats(file);
+        auto s1 = static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+        std::seed_seq seed{s1};
+        std::mt19937 mt(seed);
+        //this causes a crash apparently
+        //std::shuffle(botStats.begin(), botStats.end(), mt);
+
+        //draw players to play and start game
         std::vector<Player> players;
-        players.push_back(*(map.at(p1)()));
-        players.push_back(*(map.at(p2)()));
-        players.push_back(*(map.at(p3)()));
-        players.push_back(*(map.at(p4)()));
-        Game g = Game(players);
-        g.start();
-    }
+        for (int i = 0; i < 4; i++) {
+            players.push_back(*(map.at(botStats.at(i).first)()));
+        }
 
+        Game g = Game(players);
+        auto result = g.start();
+        for (int i = 0; i < 4; i++) {
+            botStats.at(i).second += result.at(i);
+        }
+        writeStats(file, botStats);
+    }
 
 
     return 0;
