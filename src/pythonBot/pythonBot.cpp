30c51a2c1a96cf41a9c655e7caf28578829af430
NiklasKoehnecke <niklas.koehnecke@student.hpi.de>
2019-01-22 21:02:30 +0100

    PyBot returns what is given to him from python

--- src/pythonBot/pythonBot.cpp
+++ src/pythonBot/pythonBot.cpp
@@ -1,10 +1,77 @@
 #include "pythonBot.h"
-//#include <Python.h>
+#include <Python.h>
 
 PythonBot::PythonBot() {
+    Py_Initialize();
+
+    PyObject *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+    int i;
+
+
+    std::string functionName = "main";
+    std::string programName = "pythonBot";
+
+    Py_Initialize();
+    PyObject *pName = PyUnicode_DecodeFSDefault(programName.c_str());
+    /* Error checking of pName left out */
+    /* set search path for learning.py */
+    PyObject *sysPath = PySys_GetObject("path");
+    if(sysPath != nullptr) {
+        PyList_Append(sysPath, PyUnicode_DecodeFSDefault("../src/pythonBot/"));
+    }
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pName == nullptr) {
+        fprintf(stderr, "Libname not found\n");
+        return;
+    }
+
+
+    if (pModule != NULL) {
+        pFunc = PyObject_GetAttrString(pModule, functionName.c_str());
+        /* pFunc is a new reference */
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+
+            PyObject *pArgs = pArgs = PyTuple_New(1);
+            pValue = PyUnicode_FromString("H10");
+            PyTuple_SetItem(pArgs, 0, pValue);
+
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+            if (pValue != NULL) {
+                printf("Result of call: %s\n", PyUnicode_AsUTF8(pValue));
+                Py_DECREF(pValue);
+            }
+            else {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+            }
+        }
+        else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", functionName.c_str());
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    }
+    else {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n",programName.c_str());
+    }
+    if (Py_FinalizeEx() < 0) {
+    }
 }
 
+
 PythonBot::~PythonBot() {
+    Py_Finalize();
 }
 
 Card PythonBot::nextRound() {
---
+++ src/pythonBot/pythonBot.py
@@ -0,0 +1,3 @@
+def main(card):
+
+    return card
